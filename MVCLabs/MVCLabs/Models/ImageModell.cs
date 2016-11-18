8f492b23042a60118a283cf00f9e7e5db775aac3
Hampus <Hampus@DESKTOP-6CRJOHR>
2016-11-18 13:12:41 +0100

    Added modell

---
+++ MVCLabs/MVCLabs/Models/ImageModell.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCLabs.Models
+{
+    public class ImageModell
+    {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+    }
+}
